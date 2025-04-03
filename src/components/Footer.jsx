fab5d2cc773d870f87601211e2c31ca32b33372a
brianmachado18 <andrs.machado1@gmail.com>
2025-04-03 11:38:10 -0300

    Cambios_nombre

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 export default function Footer() {
   return (
     <footer className="bg-dark text-white text-center py-4 mt-0">
-      <p className="mb-1">&copy; 2025 VentaPasajes. Todos los derechos reservados.</p>
-      <small>Contacto: info@ventapasajes.com | Tel: 0800 1234</small>
+      <p className="mb-1">&copy; 2025 AllaVoy. Todos los derechos reservados.</p>
+      <small>Contacto: info@AllaVoy.com | Tel: 0800 1234</small>
     </footer>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 export default function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark px-4">
-      <Link className="navbar-brand" to="/">VentaPasajes</Link>
+      <Link className="navbar-brand" to="/">AllaVoy</Link>
       <div className="collapse navbar-collapse">
         <ul className="navbar-nav ms-auto">
           <li className="nav-item">
